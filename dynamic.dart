65933314a73edaf52e1d9c18aab69682647710e9
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-07 11:23:51 +0700

    Create dynamic.dart

---
+++ dynamic.dart
@@ -0,0 +1,14 @@
+//INI ADALAH DYNAMIC DI MANA GUNA NYA AGAR BISA MENAMPUNG SEMUA JENIS TIPE DATA
+void main(){
+  dynamic variabel = 12;
+   print(variabel);
+
+  variabel = true;
+  print(variabel);
+
+  variabel = 1.12;
+  print(variabel);
+
+  variabel = 'muhammadRafiqi';
+   print(variabel);
+ }
